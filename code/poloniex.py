0f75dcda7cb787bc306a2958e8f3ca18288c8a17
m0Rya <s14512rt@sfc.keio.ac.jp>
2017-09-05 03:35:32 +0900

    add polo

---
+++ code/poloniex.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+url = 'https://poloniex.com/public?command='
+
+
+'''
+url += 'returnTicker'
+r = requests.get(url)
+print("I got data")
+print(r.json())
+
+data = r.json()
+
+for v in data.keys():
+    n = abs(float(data[v]['lowestAsk']) - float(data[v]['highestBid']))
+    print("{0} : {1}".format(v, n))
+'''
+
